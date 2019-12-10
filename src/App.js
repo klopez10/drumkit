cf8cf0d57da3bd50a460d03c5a6175d05101c845
Karinna Lopez <klopez10@eagles.ewu.edu>
2019-12-10 09:41:13 -0800

    Added JavaScript from staging.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const data = [
+  { id: 'Approve Message', letter: 'A', src: 'https://drive.google.com/open?id=1zJqphM4QuKjXWYOrpItzNFn0MOeCUTns' },
+  { id: 'Assistant Manager', letter: 'S', src: 'http://billor.chsh.chc.edu.tw/sound/bass4.wav' },
+  { id: 'Doubt', letter: 'D', src: 'http://www.denhaku.com/r_box/tr707/closed.wav' },
+	{ id: 'Farmer Market', letter: 'F', src: 'http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav' },
+  { id: 'Bass', letter: 'G', src: 'http://billor.chsh.chc.edu.tw/sound/bass4.wav' },
+  { id: 'How to Breathe', letter: 'H', src: 'http://www.denhaku.com/r_box/tr707/closed.wav' },
+	{ id: 'Naughty', letter: 'J', src: 'http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav' },
+  { id: 'Ok', letter: 'K', src: 'http://billor.chsh.chc.edu.tw/sound/bass4.wav' }
+]
+
+class DrumPad extends React.Component {
+ 
+  componentDidMount() {
+    console.log(this.audio)
+    document.addEventListener('keydown', this.handleKeydown)
+    window.focus()
+  }
+  
+  componentWillUnmount() {
+   document.removeEventListener('keydown', this.handleKeydown)
+  }
+  
+  handleKeydown = e => {
+    if(e.keyCode === this.props.letter.charCodeAt()) {
+      this.audio.play()
+      this.audio.currentTime = 0
+      this.props.handleDisplay(this.props.id)
+    }
+  }
+ 
+  handleClick = () => {
+    this.audio.play()
+    this.audio.currentTime = 0
+    this.props.handleDisplay(this.props.id)
+  }
+  
+  render() {
+    return (
+      <div 
+          className='drum-pad' 
+          id={this.props.id}
+          onClick={this.handleClick}
+      >
+        <h1>{this.props.letter}</h1>
+        <audio id={this.props.letter}
+               className='clip'
+               src={this.props.src}
+               ref={ref => this.audio = ref}
+          ></audio>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      display: 'Click or Press a Key'
+    }
+  }
+  
+  handleDisplay = display => this.setState({ display })
+  
+  render(){
+    return(
+    <div id='drum-machine'>
+				<div id='display'>{this.state.display}</div>
+        <div id='drum-pads'>
+					{data.map(d => (
+						<DrumPad
+							key={d.id}
+							id={d.id}
+							letter={d.letter}
+							src={d.src}
+							handleDisplay={this.handleDisplay}
+          />   
+         ))}</div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
